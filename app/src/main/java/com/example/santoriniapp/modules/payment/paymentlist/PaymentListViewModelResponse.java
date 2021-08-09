9a73e08ed86878da3a3ffe1d5902b3f2a898bb30
jonthanLucero <jonathan.lucero89@hotmail.com>
2021-08-09 12:26:58 -0500

    Add New Images and Drawable for App

---
+++ app/src/main/java/com/example/santoriniapp/modules/payment/paymentlist/PaymentListViewModelResponse.java
@@ -0,0 +1,56 @@
+package com.example.santoriniapp.modules.payment.paymentlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentListViewModelResponse
+{
+    // Error Messages.
+    public String errorMessage;
+    public Boolean isLoading  ;
+    public List<PaymentItem> paymentList;
+    public int currentTimeSpinnerPosition;
+    public ArrayList<PaymentDateRowSpinnerItem> paymentListSpinnerList;
+    public boolean isReload;
+    public boolean reloadFragmentActivitiesList;
+    public String paymentDateRequestCode;
+
+    public PaymentListViewModelResponse() {
+        this.errorMessage   = "";
+        this.isLoading      = false;
+        this.paymentList = new ArrayList<>();
+        this.reloadFragmentActivitiesList = false;
+        this.currentTimeSpinnerPosition = 0;
+        this.paymentListSpinnerList=new ArrayList<>();
+        this.isReload = false;
+        this.paymentDateRequestCode = "";
+    }
+    //public String totalCustomer(){ return itemSummary.totalCustomerCount+""; }
+
+    //public String totalPerformedActivities(){return performedActivities()+"";}
+    //public int performedActivities(){ return itemSummary.ordersCount+itemSummary.paymentsCount+itemSummary.quotesCount+itemSummary.returnRequestCount+itemSummary.visitsCount; }
+    public boolean showNoContentFound(){
+        return  !showContentFound()&&!isLoading;
+    }
+    public boolean showContentFound(){
+        return  (paymentList.size() >0)?true:false;
+    }
+
+    /*
+    public String showMessage(){
+        String message ="";
+        if (paymentList.size()>0){
+            PaymentDateRowSpinnerItem itemCurrentSpinner = paymentListSpinnerList.get(this.currentTimeSpinnerPosition );
+            message = "No se han encontrado gestiones para el rango de fecha escogido ("+itemCurrentSpinner.dateTitle()+": "+itemCurrentSpinner.dateName()+")";
+
+        }
+        return (!this.errorMessage.trim().isEmpty())?this.errorMessage:message;
+    }
+
+     */
+
+    public boolean showAddPaymentButton()
+    {
+        return !isLoading;
+    }
+}
